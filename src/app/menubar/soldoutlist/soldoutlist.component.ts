655d18fdd0f2149ac4ec40d83a9c4a3c958d0ed4
vivek-chandu <vivek-chandu@priyanet.com>
2022-08-25 12:42:15 +0530

    soldoutlist

--- src/app/menubar/soldoutlist/soldoutlist.component.ts
+++ src/app/menubar/soldoutlist/soldoutlist.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@azure/core-http';
+import { NgxSpinnerService } from 'ngx-spinner';
 import { FmsService } from 'src/app/services/fms.service';
 
 @Component({
@@ -10,13 +11,17 @@ import { FmsService } from 'src/app/services/fms.service';
 export class SoldoutlistComponent implements OnInit {
   soldoutList: any;
 
-  constructor(private http:FmsService) { }
+  constructor(private http: FmsService,public spinnerService: NgxSpinnerService) { }
 
   ngOnInit(): void {
+    this.getList()
   }
-getList(){
-this.http.soldOutList().subscribe(res=>{
-this.soldoutList=res;
-})
-}
+  getList() {
+    this.spinnerService.show()
+    this.http.soldOutList().subscribe(res => {
+      this.soldoutList = res;
+      this.spinnerService.hide()
+      console.log(this.soldoutList)
+    })
+  }
 }
